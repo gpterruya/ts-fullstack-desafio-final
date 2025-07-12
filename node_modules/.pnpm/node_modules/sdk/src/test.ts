7b28e89edad1259bd7d497e001c51f2909941973
gpterruya <68238033+gpterruya@users.noreply.github.com>
2025-07-12 12:41:56 -0300

    feat(sdk): implementa chamadas tipadas para backend

---
+++ node_modules/.pnpm/node_modules/sdk/src/test.ts
@@ -0,0 +1,9 @@
+import { sdk } from ".";
+
+(async () => {
+  const usuario = await sdk.getUsuario();
+  console.log("Usuário:", usuario);
+
+  const produto = await sdk.getProduto();
+  console.log("Produto:", produto);
+})();
